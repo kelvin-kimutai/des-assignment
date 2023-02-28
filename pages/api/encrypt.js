8d6e1121ffff47cca480b0998b4f018b465576c3
kelvin-kimutai <kelvinkpkrr@gmail.com>
2023-02-28 16:18:28 +0300

    Update

--- pages/api/encrypt.js
+++ pages/api/encrypt.js
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+import emailjs from "@emailjs/browser";
 import crypto from "crypto";
 
 function encrypt(message, key) {
@@ -8,41 +8,49 @@ function encrypt(message, key) {
   return ciphertext;
 }
 
+async function sendSMS(mobile_number, key) {
+  await fetch(process.env.NEXT_PUBLIC_SMS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "api-key": process.env.NEXT_PUBLIC_SMS_API_KEY,
+    },
+    body: JSON.stringify({ to: mobile_number, message: key }),
+  }).catch((e) => console.log(e));
+}
+
+async function sendEmail(email, message) {
+  const API_KEY = process.env.NEXT_PUBLIC_MAILJET_API_KEY;
+  const DOMAIN = "YOUR_DOMAIN_NAME";
+
+  const formData = require("form-data");
+  const Mailgun = require("mailgun.js");
+
+  const mailgun = new Mailgun(formData);
+  const client = mailgun.client({ username: "api", key: API_KEY });
+
+  const messageData = {
+    from: "Excited User <me@samples.mailgun.org>",
+    to: email,
+    subject: "Secret Message",
+    text: message,
+  };
+
+  await client.messages
+    .create(DOMAIN, messageData)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
 export default async function handler(req, res) {
   const { message, key, email, mobile_number } = req.body;
 
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-
-  // encrypt plain text using DES algorithm
   const encryptedMessage = encrypt(message, key);
+  await sendEmail(email, message);
+  await sendSMS(mobile_number, key);
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Secret Organization 👻" <admin@secret.com>', // sender address
-    to: email, // list of receivers
-    subject: "Secret Message", // Subject line
-    text: encryptedMessage, // plain text body
-    html: `<b>${encryptedMessage}</b>`, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   res.status(200).json({ message: encryptedMessage });
 }
